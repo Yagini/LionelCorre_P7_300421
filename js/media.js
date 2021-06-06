cd5998ee14e8fe2afeda0ffbd56c3783925475a9
Yagini <corre.lionel@gmail.com>
2021-06-06 20:27:08 +0400

    build final

--- js/media.js
+++ js/media.js
@@ -1,7 +1,7 @@
 import { manageTags } from "./tags-search.js";
 
 export const createMedia = (recipes) => {
-  addMedia(recipes);  
+  addMedias(recipes);  
   search2(recipes);
 };
 
@@ -17,18 +17,20 @@ export const createMedia = (recipes) => {
  */
 
  const search2 = (recipes) => {
-  const searchInput = document.getElementById("search");  
-  searchInput.addEventListener("keyup", () => {
+  const searchInput = document.getElementById("search");
+  const error = document.getElementById("media__error");  
+  searchInput.addEventListener("keyup", () => {     
     const filteredRecipes = getResults(recipes, searchInput.value.toLowerCase());
-    addMedia(filteredRecipes);
+    addMedias(filteredRecipes);
     manageTags(filteredRecipes);
+    error.style.display = filteredRecipes.length === 0 ? 'block' : 'none';
   });     
 };
 
 /**
  * Fonction qui se charge de filtrer dans les éléments du tableaux (name, ingredients, description).
  * La méthode filter créer et retourne un nouveau tableau contenant tous les éléments du tableau d'origine
- * grâce a l'appelle de la fonction callback
+ * en écartant les éléments qui ne correspondent pas l'appelle de la fonction callback
  * @param {array} recipes
  * @param {string} input
  * @returns
@@ -55,71 +57,74 @@ const getResults = (recipes, input) => {
  * @param {array} recipes
  */
 
-const addMedia = (recipes) => {
+const addMedias = (recipes) => {
   const mediaContent = document.querySelector(".media__content");
   mediaContent.innerHTML = "";
+  recipes.forEach(addMedia);
+}
 
-  recipes.forEach((recipe) => {
-    const mediaBlock = document.createElement("article");
-    mediaBlock.classList.add("media__block");
+const addMedia = (recipe) => {
+  const mediaContent = document.querySelector(".media__content");
+  
+  const mediaBlock = document.createElement("article");
+  mediaBlock.classList.add("media__block");
 
-    const mediaImage = document.createElement("div");
-    mediaImage.classList.add("media__img");
+  const mediaImage = document.createElement("div");
+  mediaImage.classList.add("media__img");
 
-    const mediaRecipe = document.createElement("section");
-    mediaRecipe.classList.add("media__recipe");
+  const mediaRecipe = document.createElement("section");
+  mediaRecipe.classList.add("media__recipe");
 
-    const mediaHeading = document.createElement("div");
-    mediaHeading.classList.add("media__heading");
+  const mediaHeading = document.createElement("div");
+  mediaHeading.classList.add("media__heading");
 
-    const mediaTitle = document.createElement("h2");
-    mediaTitle.classList.add("media__title");
-    mediaTitle.textContent = recipe.name;
+  const mediaTitle = document.createElement("h2");
+  mediaTitle.classList.add("media__title");
+  mediaTitle.textContent = recipe.name;
 
-    const mediaCookingTimer = document.createElement("div");
-    mediaCookingTimer.classList.add("media__cooking-timer");
+  const mediaCookingTimer = document.createElement("div");
+  mediaCookingTimer.classList.add("media__cooking-timer");
 
-    const mediaCookingTimerIcon = document.createElement("i");
-    mediaCookingTimerIcon.classList.add("far", "fa-clock", "media__cooking-timer--icon");
+  const mediaCookingTimerIcon = document.createElement("i");
+  mediaCookingTimerIcon.classList.add("far", "fa-clock", "media__cooking-timer--icon");
 
-    const mediaCookingTimerText = document.createElement("p");
-    mediaCookingTimerText.classList.add("media__cooking-timer--text");
-    mediaCookingTimerText.textContent = recipe.time + " min";
+  const mediaCookingTimerText = document.createElement("p");
+  mediaCookingTimerText.classList.add("media__cooking-timer--text");
+  mediaCookingTimerText.textContent = recipe.time + " min";
 
-    const mediaTask = document.createElement("div");
-    mediaTask.classList.add("media__task");
+  const mediaTask = document.createElement("div");
+  mediaTask.classList.add("media__task");
 
-    const mediaIngredients = document.createElement("ul");
-    mediaIngredients.classList.add("media__ingredients");
+  const mediaIngredients = document.createElement("ul");
+  mediaIngredients.classList.add("media__ingredients");
 
-    recipe.ingredients.forEach((ingredient) => {
-      const mediaIngredientsList = document.createElement("li");
-      mediaIngredientsList.classList.add("ingredients__list");
-      mediaIngredientsList.innerHTML = `<strong>${ingredient.ingredient}`;
-      mediaIngredientsList.innerHTML += ingredient.quantity ? ` : </strong><span>${ingredient.quantity}` : `</strong>`;
-      mediaIngredientsList.innerHTML += ingredient.unit ? ` ${ingredient.unit}</span>` : `</span>`;
-      mediaIngredients.appendChild(mediaIngredientsList);
-    });
-
-    const mediaCookingInstruction = document.createElement("p");
-    mediaCookingInstruction.classList.add("media__cooking-instruction");
-    mediaCookingInstruction.textContent = recipe.description;
-
-    mediaCookingTimer.appendChild(mediaCookingTimerIcon);
-    mediaCookingTimer.appendChild(mediaCookingTimerText);
-
-    mediaHeading.appendChild(mediaTitle);
-    mediaHeading.appendChild(mediaCookingTimer);
-
-    mediaTask.appendChild(mediaIngredients);
-    mediaTask.appendChild(mediaCookingInstruction);
-
-    mediaRecipe.appendChild(mediaHeading);
-    mediaRecipe.appendChild(mediaTask);
-
-    mediaBlock.appendChild(mediaImage);
-    mediaBlock.appendChild(mediaRecipe);
-
-    mediaContent.appendChild(mediaBlock);
+  recipe.ingredients.forEach((ingredient) => {
+    const mediaIngredientsList = document.createElement("li");
+    mediaIngredientsList.classList.add("ingredients__list");
+    mediaIngredientsList.innerHTML = `<strong>${ingredient.ingredient}`;
+    mediaIngredientsList.innerHTML += ingredient.quantity ? ` : </strong><span>${ingredient.quantity}` : `</strong>`;
+    mediaIngredientsList.innerHTML += ingredient.unit ? ` ${ingredient.unit}</span>` : `</span>`;
+    mediaIngredients.appendChild(mediaIngredientsList);
   });
+
+  const mediaCookingInstruction = document.createElement("p");
+  mediaCookingInstruction.classList.add("media__cooking-instruction");
+  mediaCookingInstruction.textContent = recipe.description;
+
+  mediaCookingTimer.appendChild(mediaCookingTimerIcon);
+  mediaCookingTimer.appendChild(mediaCookingTimerText);
+
+  mediaHeading.appendChild(mediaTitle);
+  mediaHeading.appendChild(mediaCookingTimer);
+
+  mediaTask.appendChild(mediaIngredients);
+  mediaTask.appendChild(mediaCookingInstruction);
+
+  mediaRecipe.appendChild(mediaHeading);
+  mediaRecipe.appendChild(mediaTask);
+
+  mediaBlock.appendChild(mediaImage);
+  mediaBlock.appendChild(mediaRecipe);
+
+  mediaContent.appendChild(mediaBlock);
 };
